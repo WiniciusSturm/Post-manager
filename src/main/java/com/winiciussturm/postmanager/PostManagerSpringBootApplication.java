11cf60b306cfb63707616fbde901d5eb8d2ee432
Winicius Sturm <winiciussturm@gmail.com>
2018-09-18 16:02:09 -0300

    Project created

---
+++ src/main/java/com/winiciussturm/postmanager/PostManagerSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.winiciussturm.postmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostManagerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostManagerSpringBootApplication.class, args);
+	}
+}
