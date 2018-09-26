b5cea1c14a8a43a05737b2eb2fec990dd29bb110
Winicius Sturm <winiciussturm@gmail.com>
2018-09-26 17:46:26 -0300

    Zipping a directory

--- src/main/java/com/winiciussturm/postmanager/PostManagerSpringBootApplication.java
+++ src/main/java/com/winiciussturm/postmanager/PostManagerSpringBootApplication.java
@@ -1,16 +1,20 @@
 package com.winiciussturm.postmanager;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.winiciussturm.postmanager.services.AudioParserService;
+import com.winiciussturm.postmanager.services.ZipService;
 
 @SpringBootApplication
 public class PostManagerSpringBootApplication {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws IOException 
 	{
 		SpringApplication.run(PostManagerSpringBootApplication.class, args);
 		AudioParserService.listSongData();
+		ZipService.compress("C:/Users/winicius/Desktop/Personal/Zip");
 	}
 }
